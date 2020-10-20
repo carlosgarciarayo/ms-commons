4aebae7cefe1a1c08c9f23c37f40356c0151da51
carlosgarciarayo201 <carlos@carlos-VirtualBox>
2020-10-20 10:18:36 +0200

    first commit

---
+++ src/test/java/com/app/ms/commons/MsCommonsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.ms.commons;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsCommonsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
