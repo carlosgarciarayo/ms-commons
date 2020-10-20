2d18435d189d8260e9294ca4c4e964fcf6e6f54f
carlosgarciarayo201 <carlos@carlos-VirtualBox>
2020-10-20 13:57:28 +0200

    commit

--- src/main/java/com/app/ms/commons/MsCommonsApplication.java
+++ src/main/java/com/app/ms/commons/MsCommonsApplication.java
@@ -1,13 +1,9 @@
 package com.app.ms.commons;
 
-import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
-public class MsCommonsApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(MsCommonsApplication.class, args);
-	}
-
-}
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
+public class MsCommonsApplication {}
